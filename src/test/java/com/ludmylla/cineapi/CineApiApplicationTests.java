e41c7a91f401a5dee317632fdaab238701001d9d
Ludmylla Arielly <ludmay4@gmail.com>
2021-07-26 18:27:08 -0300

    Commit inicial

---
+++ src/test/java/com/ludmylla/cineapi/CineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ludmylla.cineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
