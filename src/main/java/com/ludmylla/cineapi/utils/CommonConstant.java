fec982f2e8fbc8e5d30245fd55f81769e3858d3f
Ludmylla Arielly <ludmay4@gmail.com>
2021-07-27 20:29:38 -0300

    Criando classe de constantes

---
+++ src/main/java/com/ludmylla/cineapi/utils/CommonConstant.java
@@ -0,0 +1,6 @@
+package com.ludmylla.cineapi.utils;
+
+public class CommonConstant {
+    public static final String TOKEN_SPLIT = "Bearer ";
+    public static final String AUTHORIZATION = "Authorization";
+}
