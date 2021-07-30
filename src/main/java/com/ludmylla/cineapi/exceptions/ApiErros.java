169b073b07b69364488a4625d83262cf695e8799
Ludmylla Arielly <ludmay4@gmail.com>
2021-07-30 15:48:29 -0300

    Adicionando controle de exceções

---
+++ src/main/java/com/ludmylla/cineapi/exceptions/ApiErros.java
@@ -0,0 +1,29 @@
+package com.ludmylla.cineapi.exceptions;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiErros {
+
+    private List<String> erros;
+
+    public ApiErros(UserNotFoundException ex) {
+        this.erros = Arrays.asList(ex.getMessage());
+    }
+
+    public ApiErros(AccessDeniedException ex){
+        this.erros = Arrays.asList(ex.getMessage());
+    }
+
+    public List<String> getErros() {
+        return erros;
+    }
+
+    public ApiErros(List<String> erros) {
+        this.erros = erros;
+    }
+
+    public ApiErros(String messageError) {
+        this.erros = Arrays.asList(messageError);
+    }
+}
---
+++ src/main/java/com/ludmylla/cineapi/exceptions/ApplicationControllerAdvice.java
@@ -0,0 +1,36 @@
+package com.ludmylla.cineapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiErros handleMethodNotValidException(MethodArgumentNotValidException ex){
+        List<String> errors = ex.getBindingResult().getAllErrors()
+                .stream()
+                .map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
+        return new ApiErros(errors);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiErros handleUserNotFoundException(UserNotFoundException ex) {
+        return new ApiErros(ex);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ApiErros handleAccessDeniedException(AccessDeniedException ex) {
+        return new ApiErros(ex);
+    }
+}
