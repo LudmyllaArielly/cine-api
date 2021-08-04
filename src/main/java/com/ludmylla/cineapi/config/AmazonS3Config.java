9f6f96fd481f705324a1eefecfb65c6b610f854a
Ludmylla Arielly <ludmay4@gmail.com>
2021-08-04 13:01:54 -0300

    Configurando amazons3

---
+++ src/main/java/com/ludmylla/cineapi/config/AmazonS3Config.java
@@ -0,0 +1,31 @@
+package com.ludmylla.cineapi.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonS3Config {
+
+    @Value("${aws.accessKey}")
+    private String accessKey;
+
+    @Value("${aws.secretKey}")
+    private String secretKey;
+
+    @Value("${aws.region}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3(){
+        BasicAWSCredentials awsCred = new BasicAWSCredentials(accessKey, secretKey);
+        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard().withRegion(Regions.fromName(region))
+                .withCredentials(new AWSStaticCredentialsProvider(awsCred)).build();
+        return amazonS3;
+    }
+}
