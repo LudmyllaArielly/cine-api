70b84674721c164f5ddb2192672574a2931ea989
Ludmylla Arielly <ludmay4@gmail.com>
2021-09-06 11:24:27 -0300

    Adicionando validações no dtos de período

--- src/main/java/com/ludmylla/cineapi/model/dto/PeriodCreateAndListDto.java
+++ src/main/java/com/ludmylla/cineapi/model/dto/PeriodCreateAndListDto.java
@@ -2,8 +2,11 @@ package com.ludmylla.cineapi.model.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class PeriodCreateAndListDto {
 
+    @NotBlank(message = "{required.period}")
     private String periodOfStory;
 }
--- src/main/java/com/ludmylla/cineapi/model/dto/PeriodUpdateDto.java
+++ src/main/java/com/ludmylla/cineapi/model/dto/PeriodUpdateDto.java
@@ -2,9 +2,15 @@ package com.ludmylla.cineapi.model.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class PeriodUpdateDto {
 
+    @NotNull(message = "{notnull.id}")
     private Long id;
+
+    @NotBlank(message = "{required.period}")
     private String periodOfStory;
 }
